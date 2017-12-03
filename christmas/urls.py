00763cb4c9057563ffa3ab937e33201083275d70
Dylan Staatz <dms9800@gmail.com>
2017-12-02 20:39:39 -0600

    Added the ability to hide inputs

--- christmas/urls.py
+++ christmas/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^(?P<group_id>[0-9A-Za-z]+)/invite/$', InviteView.as_view(), name='invite'),
     url(r'^(?P<group_id>[0-9A-Za-z]+)/create/$', ListCreateView.as_view(), name='create_list'),
     url(r'^(?P<group_id>[0-9A-Za-z]+)/(?P<list_id>[0-9]+)/$', ListUpdateView.as_view(), name='view_list'),
-    # url(r'^(?P<group_id>[0-9A-Za-z]+)/(?P<list_id>[0-9]+)/$', ListUpdateView.as_view(), name='view_list'),
+    url(r'^(?P<group_id>[0-9A-Za-z]+)/(?P<list_id>[0-9]+)/create/$', ItemCreateView.as_view(), name='create_item'),
+    url(r'^(?P<group_id>[0-9A-Za-z]+)/(?P<list_id>[0-9]+)/(?P<item_id>[0-9]+)/$', ItemUpdateView.as_view(), name='view_item'),
 ]
--- christmas/views.py
+++ christmas/views.py
@@ -6,6 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
 
 from .models import *
+from .forms import *
 
 # Create your views here.
 class HomepageView(TemplateView):
@@ -43,7 +44,7 @@ class GroupUpdateView(UpdateView):
 class ListCreateView(CreateView):
     
     model = List
-    fields = ['name', 'group_address']
+    form_class = ListCreateForm
     template_name_suffix = '_create_form'
 
     def get_initial(self, **kwargs):
@@ -64,5 +65,29 @@ class ListUpdateView(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(ListUpdateView, self).get_context_data(**kwargs)
         context['address'] = self.kwargs['group_id']
-        context['object_list'] = list(get_object_or_404(List, id=self.kwargs['list_id']).items)
+        print(type(get_object_or_404(List, id=self.kwargs['list_id']).items))
+        # context['object_list'] = 
         return context
+
+
+class ItemCreateView(CreateView):
+    
+    model = Item
+    form_class = ItemCreateForm
+    template_name_suffix = '_create_form'
+
+    def get_initial(self, **kwargs):
+        newInitial = self.initial
+        newInitial['group_address'] = self.kwargs['group_id']
+        newInitial['parent_id'] = self.kwargs['list_id']
+        return newInitial
+
+
+class ItemUpdateView(UpdateView):
+    
+    model = Item
+    fields = ['title', 'description', 'link']
+    template_name_suffix = '_update_form'
+
+    def get_object(self, queryset=None):
+        return get_object_or_404(Item, id=self.kwargs['item_id'])
